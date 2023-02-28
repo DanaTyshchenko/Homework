36341aef98ee0bc77c0e73624d8876ebd5b0256b
Dana <danasorokopud@gmail.com>
2023-02-28 12:51:01 +0400

    first commit

---
+++ Homework_1/Exercise_2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход 
+//три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.WriteLine("Введите первое число:");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число:");
+int number_3 = Convert.ToInt32(Console.ReadLine());
+
+if (number_1 > number_2)
+   
+    if (number_1 > number_3)
+    Console.WriteLine(number_1);
+    else 
+    Console.WriteLine(number_3);
+   
+if (number_2 > number_3)
+Console.WriteLine(number_2);
+
+else 
+    Console.WriteLine(number_3);
+
+
+    
+    
+    
---
+++ Homework_1/Exercise_4/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int index = 1;
+while (index <= n)
+{
+   if (index % 2 == 0)
+        if (index == n || index == n - 1)
+        Console.WriteLine(index);
+        else
+        Console.Write(index + ", ");
+    index = index + 1;
+}
+    Console.WriteLine();
+
+
+
+
+   
+    
+
+    
+
+  
+
+
